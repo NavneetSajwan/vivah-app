239721036e99c9043c2a8b2449db621856956560
NavneetSajwan <navneetsinghsajwan@gmail.com>
2023-08-17 00:06:18 +0530

    Initialize project using Create React App

---
+++ Register.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleRegister = () => {
+    // Implement registration logic here
+    // You would typically make an API call to your backend
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={formData.name}
+        onChange={handleInputChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleInputChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
+
+export default Register;
